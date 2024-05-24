4dc4bf3acd794a18e369363c83023b9619c03116
Grovert11 <g.wijngaards@student.fontys.nl>
2024-05-24 14:44:46 +0200

    Removed best practice main code

--- shimmer_run.py
+++ shimmer_run.py
@@ -3,10 +3,8 @@ import time
 from shimmer import ShimmerDevice
 
 # Demo of how to use shimmer.py to use the Shimmer for data collection
-if __name__ == '__main__':
-    device = ShimmerDevice('COM3')
-    device.start_streaming()
-    time.sleep(10.0)
-    device.stop_streaming()
 
-exit(0)
+device = ShimmerDevice('COM9')
+device.start_streaming()
+time.sleep(1.0)
+device.stop_streaming()
