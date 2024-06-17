cb35c8d2a8d4b4399716311ecb5c8234c0722159
NickFriendship <n.friendship@outlook.com>
2024-06-17 09:50:59 +0200

    met donker

--- mock-up-psv.py
+++ mock-up-psv.py
@@ -2,9 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
+import pyodbc
+import config
 import time
+import shimmer
 from shimmer import ShimmerDevice
 from shimmer import convert_ADC_to_GSR  # Import the conversion function
+import neurokit2 as nk
 
 # Initialize or update session state
 if "disabled" not in st.session_state:
@@ -14,7 +18,7 @@ if "device" not in st.session_state:
     st.session_state.device = None
 
 if "line_chart_data" not in st.session_state:
-    st.session_state.line_chart_data = pd.DataFrame(columns=["timestamp", "gsr_raw", "ppg_raw"])
+    st.session_state.line_chart_data = pd.DataFrame(columns=["timestamp", "gsr", "ppg_raw"])
 
 if "annotations_df" not in st.session_state:
     st.session_state.annotations_df = pd.DataFrame(columns=["timestamp", "value", "y"])
@@ -26,7 +30,7 @@ st.set_page_config(layout="wide", page_title="PSV Mindgames Dashboard", page_ico
 st.header('Dashboard Mindgames - PSV', divider='red')
 
 # Create tabs
-tab1, tab2, tab3 = st.tabs(["Live monitoring", "Historical HRV", "Historical GSR"])
+tab1, tab2 = st.tabs(["Live monitoring", "Historical data"])
 
 with tab1:
     
@@ -55,7 +59,7 @@ with tab1:
             new_annotation = {
                 'timestamp': st.session_state.line_chart_data.iloc[-1]['timestamp'], 
                 'value': ping_text, 
-                'y': st.session_state.line_chart_data.iloc[-1]['gsr_raw']
+                'y': st.session_state.line_chart_data.iloc[-1]['gsr']
             }
             st.session_state.annotations_df = st.session_state.annotations_df.append(new_annotation, ignore_index=True)
             st.toast('Ping sent', icon="🎉")
@@ -70,24 +74,18 @@ with tab1:
             if seconds > 0:  # To prevent initial duplicate data generation             
                 # Append livestreamed values to DataFrame
                 live_data = st.session_state.device.get_live_data()
-                
-                # Convert GSR raw values to integers
-                live_data['gsr_raw'] = live_data['gsr_raw'].astype(int)
-                
-                # Apply GSR conversion to the raw GSR values
-                #live_data['gsr_raw'] = live_data['gsr_raw'].apply(convert_ADC_to_GSR)
-                
+                                
                 st.session_state.line_chart_data = pd.concat([st.session_state.line_chart_data, live_data]).drop_duplicates().reset_index(drop=True)
                 
                 # Keep only the last 40 datapoints
-                st.session_state.line_chart_data = st.session_state.line_chart_data.tail(100)
+                st.session_state.line_chart_data = st.session_state.line_chart_data.tail(200)
 
                 # Ensure annotations are in sync with the live data
                 annotations_data_tail = st.session_state.annotations_df[st.session_state.annotations_df['timestamp'] >= st.session_state.line_chart_data['timestamp'].min()]
 
                 # Build the GSR line chart
                 gsr_chart = alt.Chart(st.session_state.line_chart_data).transform_fold(
-                    ["gsr_raw"],
+                    ["gsr"],
                     as_=['Measurement', 'value']
                 ).mark_line().encode(
                     x=alt.X('timestamp:T', axis=alt.Axis(title='Timestamp')),
@@ -125,3 +123,159 @@ with tab1:
                 st.session_state.device = None
                 st.toast('Shimmer disconnected', icon="🔌")
                 break
+
+with tab2:
+    # Database connection function
+    def get_db_connection():
+        try:
+            conn = pyodbc.connect(
+                f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+                f"SERVER={config.server_host};"
+                f"DATABASE=PSV;"
+                f"UID=team;"
+                f"PWD={config.password}"
+            )
+            st.write("Database connection successful")
+            return conn
+        except pyodbc.Error as e:
+            st.error(f"Database connection failed: {e}")
+            st.stop()
+
+    # Fetch sensor data from the database
+    def fetch_sensor_data(conn):
+        query = "SELECT * FROM dbo.sensor_data"
+        sensor_data = pd.read_sql(query, conn)
+        sensor_data['gsr'] = sensor_data['gsr_raw'].apply(shimmer.convert_ADC_to_GSR)
+        return sensor_data
+
+
+    # Fetch measurement data from the database
+    def fetch_measurement_data(conn):
+        query = "SELECT * FROM dbo.measurement"
+        measurement_data = pd.read_sql(query, conn)
+        return measurement_data
+
+
+    # Fetch shimmer data from the database
+    def fetch_shimmer_data(conn):
+        query = "SELECT * FROM dbo.shimmer"
+        shimmer_data = pd.read_sql(query, conn)
+        return shimmer_data
+
+    # Main function to fetch and display data
+    def main():
+        # Create a connection to the database
+        conn = get_db_connection()
+
+    # Create a connection to the database
+    conn = get_db_connection()
+
+    # Fetch data
+    sensor_data = fetch_sensor_data(conn)
+    measurement_data = fetch_measurement_data(conn)
+    shimmer_data = fetch_shimmer_data(conn)
+
+    # Create box with filter
+    with st.expander("Filter"):
+        col1, col2, col3, col4 = st.columns(4, gap="large")
+        with col1:
+            start_date = st.date_input("Start date", sensor_data['datetime'].min().date())
+        with col2:
+            end_date = st.date_input("End date", sensor_data['datetime'].max().date())
+        with col3:
+            st.selectbox('Training type', ("aristotle", "MoveSense"), index=None)
+        with col4:
+            st.selectbox('Player', ("Luuk de Jong", "Een andere speler van PSV"), index=None)
+
+    # Filter data based on user input
+    filtered_data = sensor_data[
+        (sensor_data['datetime'].dt.date >= start_date) & (sensor_data['datetime'].dt.date <= end_date)]
+
+    sampling_rate = 100
+
+    peaks, info = nk.ppg_peaks(filtered_data['ppg_raw'], sampling_rate=sampling_rate)
+    hrv_time = nk.hrv_time(peaks, sampling_rate=sampling_rate, show=True)
+
+    st.dataframe(hrv_time)
+
+    # Create columns for metrics
+    col1, col2, col3, col4, col5 = st.columns(5, gap="large")
+
+    # Display average Heart rate in a box
+    average_heart = filtered_data['ppg_raw'].mean()
+    col1.metric("Average Heart rate", f"{average_heart:.0f} bpm")
+
+    # Display max HRV in a box
+    max_hrv = filtered_data['ppg_raw'].max()
+    col2.metric("Max HRV", f"{max_hrv:.0f} ms")
+
+    # Display minimum HRV in a box
+    min_hrv = filtered_data['ppg_raw'].min()
+    col3.metric("Min HRV", f"{min_hrv:.0f} ms")
+
+    # Display average HRV in a box
+    average_hrv = filtered_data['ppg_raw'].mean()
+    col4.metric("Average HRV", f"{average_hrv:.0f} ms")
+
+    # Create a selection interval for the date range slider
+    date_range = alt.selection_interval(bind='scales', encodings=['x', 'y'])
+
+    # Create an Altair line chart with the filtered data and add the selection
+    alt_chart = alt.Chart(filtered_data).mark_line().encode(
+        x='datetime:T',
+        y='gsr:Q',
+        tooltip=['datetime', 'gsr']
+    ).add_selection(
+        date_range
+    ).properties(
+        title='GSR (galvanic skin response)'
+    )
+
+    # Display the Altair chart
+    st.altair_chart(alt_chart, use_container_width=True)
+
+    # Create an Altair line chart with the filtered data and add the selection
+    raw_ppg = alt.Chart(filtered_data).mark_line().encode(
+        x='datetime:T',
+        y='ppg_raw:Q',
+        tooltip=['datetime', 'ppg_raw']
+    ).add_selection(
+        date_range
+    ).properties(
+        title='PPG (galvanic skin response)'
+    )
+
+    # Display the Altair chart
+    st.altair_chart(raw_ppg, use_container_width=True)
+
+
+    #Create a Plotly line chart with a date range slider
+    #fig = px.line(filtered_data, x='datetime', y='gsr_raw', title='GSR (galvanic skin response)')
+
+    #fig.update_xaxes(rangeslider_visible=True)
+
+    #Display the Plotly figure
+    #st.plotly_chart(fig)
+
+    # Merge the tables to create a complete dataset
+    merged_data = pd.merge(sensor_data, shimmer_data, left_on="shimmer_id", right_on="id")
+    merged_data = pd.merge(merged_data, measurement_data, left_on="shimmer_id", right_on="shimmer_id")
+
+    # Calculate the average GSR per event
+    average_gsr_per_event = merged_data.groupby('event')['gsr'].mean().reset_index()
+
+    # Display the results
+    st.header('Average GSR per Event')
+    st.dataframe(average_gsr_per_event)
+
+    if __name__ == "__main__":
+        main()
+
+        # Calculate average RRMSSD per player
+        #average_rrmssd_per_player = sensor_data.groupby('PlayerID')['RMSSD'].mean().reset_index()
+
+        # Create a bar chart for average RRMSSD per player
+        #fig = px.bar(average_rrmssd_per_player, x='PlayerID', y='RMSSD', title='Average HRV per Player')
+
+        # Display the bar chart in Streamlit
+        #st.plotly_chart(fig)
--- mock-up-psv_OUD.py
+++ mock-up-psv_OUD.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import altair as alt
 import time
+from shimmer import ShimmerDevice
+import pyodbc
+import config
 
 # Wide page
 st.set_page_config(layout="wide", page_title="PSV Mindgames Dashboard", page_icon="⚽")
@@ -11,7 +14,24 @@ st.set_page_config(layout="wide", page_title="PSV Mindgames Dashboard", page_ico
 st.header('Dashboard Mindgames - PSV', divider='red')
 
 # Create tabs
-tab1, tab2, tab3 = st.tabs(["Live monitoring", "Historical HRV", "Historical GSR"])
+tab1, = st.tabs(["Live monitoring"])
+
+# Database connection function
+def get_db_connection():
+    conn = pyodbc.connect(
+        f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+        f"SERVER={config.server_host};"
+        f"DATABASE=PSV;"
+        f"UID=team;"
+        f"PWD={config.password}"
+    )
+    return conn
+
+# Fetch sensor data from the database
+def fetch_sensor_data(conn):
+    query = "SELECT * FROM dbo.sensor_data"
+    data = pd.read_sql(query, conn)
+    return data
 
 with tab1:
     # Initialize or update session state
@@ -20,7 +40,7 @@ with tab1:
 
     if "line_chart_data" not in st.session_state:
         # Initialize line chart data
-        st.session_state.line_chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["HRV", "PPM", "Skin Conductance"])
+        st.session_state.line_chart_data = pd.DataFrame(columns=["timestamp", "GSR", "HRV"])
 
     # Form to start monitoring
     with st.form('start_form'):
@@ -29,14 +49,20 @@ with tab1:
             st.selectbox('Game', ("Aristotle", "MoveSense"), index=None)
         with col2:
             st.selectbox('Player', ("Luuk de Jong", "Een andere speler van PSV"), index=None)
-        submit_button = st.form_submit_button("Start", on_click=lambda: setattr(st.session_state, 'disabled', True), disabled=st.session_state.disabled)
+        submit_button = st.form_submit_button("Start", on_click=lambda: setattr(st.session_state, 'disabled', True),
+                                              disabled=st.session_state.disabled)
 
     # Annotations setup
     annotations = [(10, "Speler mist de bal"), (13, "Speler schrikt van onweer"), (19, "Speler moet niezen")]
-    annotations_df = pd.DataFrame(annotations, columns=["index", "event"])
+    annotations_df = pd.DataFrame(annotations, columns=["timestamp", "event"])
     annotations_df["y"] = 0
 
-    if submit_button or st.session_state.disabled == True:
+    if submit_button or st.session_state.disabled:
+
+        # Start streaming
+        device = ShimmerDevice('COM3')
+        device.start_streaming()
+        st.toast('Shimmer connected', icon="🎉")
 
         # Ping form
         with st.form('ping_form', clear_on_submit=True):
@@ -44,49 +70,69 @@ with tab1:
             submit_ping = st.form_submit_button("Send ping")
 
         if submit_ping:
-            st.session_state.line_chart_data.loc[len(st.session_state.line_chart_data)] = np.random.randn(3)
-            annotations_df = annotations_df.append({'index': len(st.session_state.line_chart_data) - 1, 'event': ping_text, 'y': 0}, ignore_index=True)
+            annotations_df = annotations_df.append(
+                {'timestamp': st.session_state.line_chart_data["timestamp"].iloc[-1], 'event': ping_text, 'y': 0},
+                ignore_index=True)
             st.toast('Ping sent', icon="🎉")
-        
+
         colu1, colu2, colu3 = st.columns([1, 1, 0.2])
         with colu3:
             stop_button = st.button('Stop streaming', type="primary")
-            if stop_button:
-                st.stop()
 
         placeholder = st.empty()
         # Continuous data generation loop
-        for seconds in range(200):
+        for seconds in range(21):  # Limited to 20 seconds
             if seconds > 0:  # To prevent initial duplicate data generation
-                # Generating random data for each second
-                line_chart_data_more = pd.DataFrame(np.random.randn(1, 3), columns=["HRV", "PPM", "Skin Conductance"])
-                # Append new data to existing DataFrame
-                st.session_state.line_chart_data = pd.concat([st.session_state.line_chart_data, line_chart_data_more], ignore_index=True)
+                # Append livestreamed values to DataFrame
+                live_data = device.get_live_data()
+
+                # Debug: Print live_data structure
+                st.write("Live Data:", live_data)
+
+                # Convert live_data to DataFrame if it is a list of dictionaries
+                if isinstance(live_data, list):
+                    new_data = pd.DataFrame(live_data)
+                    if not new_data.empty and all(col in new_data.columns for col in ["timestamp", "gsr_raw", "ppg_raw"]):
+                        new_data.rename(columns={"gsr_raw": "GSR", "ppg_raw": "HRV"}, inplace=True)
+                        st.session_state.line_chart_data = pd.concat(
+                            [st.session_state.line_chart_data, new_data], ignore_index=True)
+                    else:
+                        st.error("Unexpected data columns in live_data. Please check the data structure.")
+                else:
+                    st.error("Unexpected data format from device. Please check the data structure.")
+                    break
 
             # Select the last 20 rows for both line chart data and annotations
-            line_chart_data_tail = st.session_state.line_chart_data.reset_index().tail(20)
-            annotations_df_tail = annotations_df[annotations_df['index'] >= line_chart_data_tail['index'].min()]
+            line_chart_data_tail = st.session_state.line_chart_data.tail(20)
+            annotations_df_tail = annotations_df[annotations_df['timestamp'] >= line_chart_data_tail['timestamp'].min()]
 
-            # Update the line chart
-            base_line_chart = alt.Chart(line_chart_data_tail).transform_fold(
-                ["HRV", "PPM", "Skin Conductance"],
-                as_=['Measurement', 'value']
-            ).mark_line().encode(
-                x='index:Q',
-                y='value:Q',
-                color='Measurement:N'
+            # Update the GSR line chart
+            gsr_chart = alt.Chart(line_chart_data_tail).mark_line().encode(
+                x='timestamp:T',
+                y=alt.Y('GSR:Q', title='GSR'),
+                color=alt.value('blue')
+            ).interactive()
+
+            # Update the HRV line chart
+            hrv_chart = alt.Chart(line_chart_data_tail).mark_line().encode(
+                x='timestamp:T',
+                y=alt.Y('HRV:Q', title='HRV'),
+                color=alt.value('red')
             ).interactive()
 
             # Update annotations to move with the data
             annotation_layer = (
                 alt.Chart(annotations_df_tail)
-                .mark_text(size=25, text="⬇️", dx=0, dy=100, align="center")
-                .encode(x="index:Q", y=alt.Y("y:Q"), tooltip=["event"])
+                .mark_text(size=12, text="⬇️", dx=0, dy=-10, align="center")
+                .encode(x="timestamp:T", y=alt.Y("y:Q"), tooltip=["event"])
             )
 
-            # Show chart
-            combined_chart = base_line_chart + annotation_layer
-
+            # Show charts
             with placeholder.container():
-                st.altair_chart(combined_chart, theme=None, use_container_width=True)
+                st.altair_chart(gsr_chart + annotation_layer, theme=None, use_container_width=True)
+                st.altair_chart(hrv_chart + annotation_layer, theme=None, use_container_width=True)
                 time.sleep(1)
+
+            if stop_button or seconds == 20:  # Stop streaming after 20 seconds
+                device.stop_streaming()
+                break
